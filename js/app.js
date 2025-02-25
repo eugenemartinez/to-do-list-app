fbaa8cc56859e0393b95bfeddb7726f82574965d
Eugene Martinez <eugenejrmartinez@gmail.com>
2025-02-25 13:39:38 +0800

    Fix module imports

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
-// filepath: /Users/yowhenyow/Documents/Codes/task-manager-app/js/app.js
-// Import dark mode functionality
 import { initializeDarkMode } from './dark-mode.js';
-// Import default date functionality
 import { setDefaultDate } from './default-date.js';
-// Import add task functionality
 import { initializeAddTask } from './add-task.js';
-// Import checkbox functionality
 import { initializeCheckboxAll } from './header-checkbox.js';
 
 // Initialize dark mode
