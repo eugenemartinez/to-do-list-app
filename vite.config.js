47a1bd446f5ae6596feb87dca4af0f4948ead6ad
Eugene Martinez <eugenejrmartinez@gmail.com>
2025-03-06 11:57:59 +0800

    fixed build issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/to-do-list/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/to-do-list-app/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -11,5 +11,8 @@ export default defineConfig({
         drop_console: true // Remove console logs in production
       }
     }
+  },
+  server: {
+    open: true // This will open the default browser
   }
 });
